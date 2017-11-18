28f8a6e2441c36e5ba512416ea5088ada5aebc17
Batleth <lukas.kaeser@posteo.de>
2017-11-18 20:38:03 +0100

    - Spinner in der Mitte + größer

--- app/components/PictureUpload.js
+++ app/components/PictureUpload.js
@@ -5,92 +5,100 @@ import Card from 'material-ui/Card';
 import Send from 'material-ui-icons/Send';
 import Preview from './Preview';
 import axios from 'axios';
-import { CircularProgress } from 'material-ui/Progress';
+import {CircularProgress} from 'material-ui/Progress';
 import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
+import {withStyles} from 'material-ui/styles';
 
 const styles = theme => ({
-    button: {
-        margin: theme.spacing.unit,
-    }})
+    spinner: {
+        position: 'absolute',
+        height: '50%',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        margin: 'auto',
+    }
+});
 
 const endpoint = 'http://52.232.1.52:1337/directupload';
 
 class UploadScreen extends React.Component {
-  state = {
-    filesToBeSent: [],
-    picturesUploaded: false,
-    sending: false
-  };
+    state = {
+        filesToBeSent: [],
+        picturesUploaded: false,
+        sending: false
+    };
 
-  onDrop = (acceptedFiles, rejectedFiles) => {
-    let filesToBeSent = this.state.filesToBeSent;
-    filesToBeSent.push(acceptedFiles);
-    this.setState({ filesToBeSent, picturesUploaded: true });
-  };
+    onDrop = (acceptedFiles, rejectedFiles) => {
+        let filesToBeSent = this.state.filesToBeSent;
+        filesToBeSent.push(acceptedFiles);
+        this.setState({filesToBeSent, picturesUploaded: true});
+    };
 
-  handleSend = () => {
-    if (this.state.filesToBeSent.length < 1) {
-      console.log('no pictures selected ');
-    } else {
-      this.setState({ sending: true });
-      //put together request
-      let formData = new FormData(this);
-      this.state.filesToBeSent.forEach(el => {
-        formData.append('photos', el[0], el[0].name);
-      });
-      axios
-        .post(endpoint, formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data'
-          }
-        })
-        .then(response => {
-          this.props.handleResponse(response);
-          // console.log(response);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    }
-  };
+    handleSend = () => {
+        if (this.state.filesToBeSent.length < 1) {
+            console.log('no pictures selected ');
+        } else {
+            this.setState({sending: true});
+            //put together request
+            let formData = new FormData(this);
+            this.state.filesToBeSent.forEach(el => {
+                formData.append('photos', el[0], el[0].name);
+            });
+            axios
+                .post(endpoint, formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                })
+                .then(response => {
+                    this.props.handleResponse(response);
+                    // console.log(response);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+    };
 
-  handleRemove = pictures => {
-    this.setState({ filesToBeSent: pictures });
-  };
+    handleRemove = pictures => {
+        this.setState({filesToBeSent: pictures});
+    };
 
-  render() {
-    let { picturesUploaded, filesToBeSent, sending } = this.state;
-    return (
-      <div>
-        {sending ? (
-          <CircularProgress />
-        ) : (
-            <div><Button raised color="primary" onClick={this.handleSend} disabled={!picturesUploaded}>
-              <Send />
-              Send Pictures
-            </Button>
-          <Card>
-            <h2>Upload pictures here</h2>
-            <Dropzone onDrop={files => this.onDrop(files)} />
-            {picturesUploaded ? (
-              <Preview
-                pictures={filesToBeSent}
-                onPictureRemove={this.handleRemove}
-              />
-            ) : (
-              <div />
-            )}
-          </Card>
+    render() {
+        let {picturesUploaded, filesToBeSent, sending} = this.state;
+        let {classes} = this.props;
+        return (
+            <div>
+                {sending ? (
+                    <CircularProgress className={classes.spinner} size={150} thickness={7}/>
+                ) : (
+                    <div><Button raised color="primary" onClick={this.handleSend} disabled={!picturesUploaded}>
+                        <Send/>
+                        Send Pictures
+                    </Button>
+                        <Card>
+                            <h2>Upload pictures here</h2>
+                            <Dropzone onDrop={files => this.onDrop(files)}/>
+                            {picturesUploaded ? (
+                                <Preview
+                                    pictures={filesToBeSent}
+                                    onPictureRemove={this.handleRemove}
+                                />
+                            ) : (
+                                <div/>
+                            )}
+                        </Card>
+                    </div>
+                )}
             </div>
-        )}
-      </div>
-    );
-  }
+        );
+    }
 }
 
 UploadScreen.PropTypes = {
-  handleResponse: PropTypes.func
+    handleResponse: PropTypes.func
 };
 
-export default UploadScreen;
+export default withStyles(styles)(UploadScreen);
