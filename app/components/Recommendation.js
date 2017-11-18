cf641acdd9fcdaee4380d01a1610e03c583600d6
Marvin Aulenbacher <marvin.aulenbacher@mobilexag.de>
2017-11-18 18:19:34 +0100

    added recommendations component

--- app/components/Recommendation.js
+++ app/components/Recommendation.js
@@ -1,9 +1,69 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Card from 'material-ui/Card';
+import { withStyles } from 'material-ui/styles';
+import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
+import Button from 'material-ui/Button';
+import Typography from 'material-ui/Typography';
+import ThumbDown from 'material-ui-icons/ThumbDown';
 
-const Recommendation = props => {
-  return <div>hallo </div>;
+const styles = {
+  card: {
+    maxWidth: 345
+  },
+  media: {
+    height: 200
+  }
 };
 
-export default Recommendation;
+const Recommendation = props => {
+  const { classes, data } = props;
+  return (
+    <div>
+      <Card className={classes.card} key={data.hotel_id}>
+        <CardMedia
+          className={classes.media}
+          image={data['image_url']}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography type="headline" component="h2">
+            {data.name ? `${data.name}` : ''}
+          </Typography>
+          <Typography component="p">
+            {data.street && data.city ? `${data.street} in ${data.city}` : ''}
+          </Typography>
+          <Typography component="p">
+            {data.price && data.currency
+              ? `Price : ${data.price} ${data.currency}`
+              : ''}
+          </Typography>
+          <Typography component="p">
+            {data.rating_average
+              ? `Average rating: ${data.rating_average}`
+              : ''}
+          </Typography>
+          <Typography component="p">
+            {data.discount && data.discount > 0
+              ? `Available discount: ${data.discount}`
+              : ''}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button raised color="primary" className={classes.button}>
+            Book
+          </Button>
+          <Button raised color="accent" className={classes.button}>
+            <ThumbDown /> Not Interested
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
+};
+
+Recommendation.propTypes = {
+  classes: PropTypes.object.isRequired,
+  data: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Recommendation);
